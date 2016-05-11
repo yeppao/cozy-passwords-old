420d7f944d77362e839a55f76914dede3bed0cb7
Sylvain LEJEUNE <sylvain.lejeun@gmail.com>
2016-05-11 10:36:20 +0200

    Add jade templating system

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,9 @@
 var path = require('path');
 
 module.exports.index = function(req, res, next) {
-  res.status(200).sendFile(path.resolve('client/index.html'));
+  res.render('index');
 };
 
 module.exports.scripts = function(req, res, next) {
   res.status(200).sendFile(path.resolve('client/scripts'));
-}
+};
